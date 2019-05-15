77de7f7286e93e91fc2b9d164beec143c0a0e49f
Kyle Weidner <kyle.weidner14@protonmail.com>
2019-05-14 18:08:23 -0600

    Refactor Post routes and controllers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const User = require('./models/user');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // require routes
 const indexRouter = require('./routes/index');
@@ -32,9 +33,10 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 //Configure Passport and Sessions
 app.use(session({
--- controllers/posts.js
+++ controllers/posts.js
@@ -2,14 +2,43 @@ const Post = require('../models/post');
 
 module.exports = {
     // Posts Index
-    async getPosts(req, res, next) {
+    async postIndex(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts });
     },
 
-    // New Post
-    newPost(req, res, next) {
+    // Post New
+    postNew(req, res, next) {
         res.render('posts/new');
+    },
 
+    //Post Create
+    async postCreate(req, res, next) {
+        let post = await Post.create(req.body.post);
+        res.redirect(`/posts/${post.id}`);
+    },
+
+    //Post Show
+    async postShow(req, res, next) {
+        let post = await Post.findById(req.params.id);
+        res.render('posts/show', { post });
+    },
+
+    // Post Edit
+    async postEdit(req, res, next) {
+        let post = await Post.findById(req.params.id);
+        res.render('posts/edit', { post });
+    },
+
+    // Post Update
+    async postUpdate(req, res, next) {
+        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, { new: true });
+        res.redirect(`/posts/${post.id}`);
+    },
+
+    // Post Destroy
+    async postDestroy(req, res, next) {
+        let post = await Post.findByIdAndRemove(req.params.id);
+        res.redirect('/posts');
     }
 };
