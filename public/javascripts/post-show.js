fd8c6b413b934cab6aa38f019b4fc9f0b88ff276
Kyle Weidner <kyle.weidner14@protonmail.com>
2019-05-22 17:10:48 -0600

    Create boilerplate layout for all views, Add layout for post-show, Refactor ejs files

---
+++ public/javascripts/post-show.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29kZW1vZGUtaW8iLCJhIjoiY2p2eWkzYW1yMDNjaTQ2bGNkOWNpZmVraCJ9.Kvn7Mj2Cw88RDmrkO16CbQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: post.coordinates,
+    zoom: 5
+});
+
+// create a HTML element for our post location/marker
+var el = document.createElement('div');
+el.className = 'marker';
+
+// make a marker for our location and add to the map
+new mapboxgl.Marker(el)
+    .setLngLat(post.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h3>' + post.title + '</h3><p>' + post.location + '</p>'))
+    .addTo(map);
