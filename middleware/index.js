211394789348ea81f00a3ad2a711f12175488968
Kyle Weidner <kyle.weidner14@protonmail.com>
2019-06-04 18:05:38 -0600

    Add user profile image feature

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 const Review = require('../models/review');
 const User = require('../models/user');
 const Post = require('../models/post');
+const { cloudinary } = require('../cloudinary');
 
-module.exports = {
+const middleware = {
     asyncErrorHandler: (fn) =>
         (req, res, next) => {
             Promise.resolve(fn(req, res, next))
@@ -40,6 +41,7 @@ module.exports = {
             res.locals.user = user;
             next();
         } else {
+            middleware.deleteProfileImage(req);
             req.session.error = 'Incorrect current password!';
             return res.redirect('/profile');
         }
@@ -51,6 +53,7 @@ module.exports = {
         } = req.body;
 
         if(newPassword && !passwordConfirmation) {
+            middleware.deleteProfileImage(req);
             req.session.error = 'Missing password confirmation!';
             return res.redirect('/profile');
         } else if(newPassword && passwordConfirmation) {
@@ -59,11 +62,17 @@ module.exports = {
                 await user.setPassword(newPassword);
                 next();
             } else {
+                middleware.deleteProfileImage(req);
                 req.session.error = 'New passwords must match!';
                 return res.redirect('/profile');
             }
         } else {
             next();
         }
+    },
+    deleteProfileImage: async req => {
+        if(req.file) await cloudinary.v2.uploader.destroy(req.file.public_id);
     }
 };
+
+module.exports = middleware;
